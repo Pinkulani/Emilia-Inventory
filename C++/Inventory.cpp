7daee069dc2e907f896b4cab1586f90c1ea0d009
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-07-14 16:31:53 +0200

    Update Inventory.cpp

--- C++/Inventory.cpp
+++ C++/Inventory.cpp
@@ -4,10 +4,33 @@
 #include <fstream>
 using namespace std;
 
+ofstream ReadMe ("ReadMe.txt");
 ofstream TestFile ("Test.txt");
+ofstream WriteTest ("WriteTest.txt");
+
+void Test() {
+  if (ReadMe.is_open())
+  {
+    ReadMe << "Version: 0.01";
+    ReadMe.close();
+  } else cout << "Unable to open file";
+}
+
+void Write() {
+  string Input;
+  int Counter = 0;
+  WriteTest.is_open();
+  while (Counter < 10) {
+   cout << "Type something: ";
+   cin >> Input; 
+   WriteTest << Input << endl;
+   Counter++;
+  }
+  WriteTest.close();
+}
 
 int main () {
-  cout << "|- Emilia's Inventory -|" << endl << "Version: 0.01" << endl;
+  cout << "|- Emilia's Inventory -|" << endl;
   if (TestFile.is_open())
   {
     TestFile << "Hello. \n";
@@ -15,5 +38,7 @@ int main () {
     TestFile.close();
   } else cout << "Unable to open file";
   
+  Test();
+  Write();
   return 0;
 }
