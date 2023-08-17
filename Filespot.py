0c36930d04d402c147cf32a18abbd1d4c6fb697a
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-08-17 16:51:34 +0200

    Adding folder creation

--- Filespot.py
+++ Filespot.py
@@ -1,4 +1,10 @@
+import os
 from Data.StartUp import *
 
-start = StartUp()
-start.OSType()
+Start = StartUp()
+Start.OSType()
+
+# Create Folders
+Folders = ["../Pictures", "../Videos", "../Gifs"]
+for X in range(0, len(Folders)):
+    os.mkdir(Folders[X])
