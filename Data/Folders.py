8f4817d11b3b79ee10643cd63e998e53f82896e8
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-08-17 18:47:04 +0200

    Create Folder file

---
+++ Data/Folders.py
@@ -0,0 +1,13 @@
+import os
+
+class Folders(object):
+    def __init__(self):
+        pass
+    def Create(self):
+        Folders = ["../Pictures", "../Videos", "../Gifs"]
+        for X in range(0, len(Folders)):
+            if os.path.exists(Folders[X]):
+                print("Folder already exists")
+            else:
+                print("Folder created")
+                os.mkdir(Folders[X])
