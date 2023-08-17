4e31b3c39b405eaeeab323fca73d98631fdb641f
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-08-17 18:45:59 +0200

    Adding Folder creation

--- Filespot.py
+++ Filespot.py
@@ -1,10 +1,9 @@
-import os
-from Data.StartUp import *
+from Data.StartUp import StartUp
+from Data.Folders import Folders
+from Data.Files import Files
 
 Start = StartUp()
 Start.OSType()
 
-# Create Folders
-Folders = ["../Pictures", "../Videos", "../Gifs"]
-for X in range(0, len(Folders)):
-    os.mkdir(Folders[X])
+Folders = Folders()
+Folders.Create()
