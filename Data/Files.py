daef33482d0117813f01efd356c2e9e96eacbc3a
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-08-17 22:15:17 +0200

    Adding File listing

--- Data/Files.py
+++ Data/Files.py
@@ -1,5 +1,15 @@
 import os
+import pathlib
+import shutil
 
 class Files(object):
     def __init__(self):
-        pass
+        self.Path = "../"
+    def List(self):
+        print("List of Files in Directory: ")
+        List = os.listdir(self.Path)
+        for X in range(0, len(List)):
+            print(List[X])
+        
+        # Test Move
+        shutil.move("CheckParity.py", "../")
