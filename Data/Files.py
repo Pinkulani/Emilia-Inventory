df1d35fb7362a70e02cb7f02532e35e27c03f083
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-08-18 21:04:01 +0200

    Adding Extension getting

--- Data/Files.py
+++ Data/Files.py
@@ -12,4 +12,24 @@ class Files(object):
             print(List[X])
         
         # Test Move
-        shutil.move("CheckParity.py", "../")
+        #shutil.move("CheckParity.py", "../")
+    def GetExtension(self, File):
+        Extension = []
+
+        # Get Extension
+        for C in reversed(File):
+            if C == ".":
+                Extension.append(C)
+                break
+            else:
+                Extension.append(C)
+
+        # Reverse Extension into readable text
+        Extension.reverse()
+
+        # Built to String
+        ExtensionString = ""
+        for S in range(0, len(Extension)):
+            ExtensionString += Extension[S]
+
+        return ExtensionString
