5ecf4905c98a7f096cd7d671d514dc73a9028b0b
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-08-18 07:46:33 +0200

    Adding GUI to Filespot

--- Filespot.py
+++ Filespot.py
@@ -1,12 +1,27 @@
+from tkinter import *
+from tkinter import ttk
+
 from Data.StartUp import StartUp
 from Data.Folders import Folders
 from Data.Files import Files
 
 Start = StartUp()
-Start.OSType()
-
 Folders = Folders()
-Folders.Create()
-
 Files = Files()
-Files.List()
+
+Filespot = Tk()
+Filespot.title("Filespot")
+
+Style = ttk.Style()
+Style.theme_use("clam")
+
+Frame = ttk.Frame(Filespot)
+Frame.grid()
+#Frame.place(x = 0, y = 0)
+
+ttk.Button(Frame, text="Start", command=lambda: Start.OSType()).grid(column = 1, row = 0)
+ttk.Button(Frame, text="Create Folders", command=lambda: Folders.Create()).grid(column = 2, row = 0)
+ttk.Button(Frame, text="List Files", command=lambda: Files.List()).grid(column = 1, row = 1)
+ttk.Button(Frame, text="Quit", command=lambda: Filespot.destroy()).grid(column = 2, row = 1)
+
+Filespot.mainloop()
