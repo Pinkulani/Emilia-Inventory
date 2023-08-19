575f057dbb150ab3077a5a403d180fd2924b195d
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-08-19 05:37:24 +0200

    Adding sort button

--- Filespot.py
+++ Filespot.py
@@ -8,9 +8,6 @@ from Data.Files import Files
 Start = StartUp()
 Folders = Folders()
 Files = Files()
-XD = Files.GetExtension("Website.mp4")
-XP = Files.GetType(XD)
-print(XP)
 
 Filespot = Tk()
 Filespot.title("Filespot")
@@ -20,9 +17,8 @@ Style.theme_use("clam")
 
 Frame = ttk.Frame(Filespot)
 Frame.grid()
-#Frame.place(x = 0, y = 0)
 
-ttk.Button(Frame, text="Start", command=lambda: Start.OSType()).grid(column = 1, row = 0)
+ttk.Button(Frame, text="Sort", command=lambda: Files.Sort()).grid(column = 1, row = 0)
 ttk.Button(Frame, text="Create Folders", command=lambda: Folders.Create()).grid(column = 2, row = 0)
 ttk.Button(Frame, text="List Files", command=lambda: Files.List()).grid(column = 1, row = 1)
 ttk.Button(Frame, text="Quit", command=lambda: Filespot.destroy()).grid(column = 2, row = 1)
