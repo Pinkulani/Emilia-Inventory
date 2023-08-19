cdd9320ff36c2c3394549d53e03d1c39f3449c11
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-08-19 05:34:46 +0200

    Adding Sort function

--- Data/Files.py
+++ Data/Files.py
@@ -5,14 +5,12 @@ import shutil
 class Files(object):
     def __init__(self):
         self.Path = "../"
+        self.FileList = os.listdir(self.Path)
+
     def List(self):
         print("List of Files in Directory: ")
-        List = os.listdir(self.Path)
-        for X in range(0, len(List)):
-            print(List[X])
-        
-        # Test Move
-        #shutil.move("CheckParity.py", "../")
+        for X in range(0, len(self.FileList)):
+            print(self.FileList[X])
 
     def GetExtension(self, File):
         Extension = []
@@ -53,3 +51,18 @@ class Files(object):
                 return "Video"
             case _:
                 return "Other"
+            
+    def Sort(self):
+        for L in range(0, len(self.FileList)):
+            Sort = self.GetExtension(self.FileList[L])
+            Sort = self.GetType(Sort)
+
+            # Sort = File Type
+            match Sort:
+                case "Picture":
+                    shutil.move(self.FileList[L], "../Pictures")
+                case "Video":
+                    shutil.move(self.FileList[L], "../Videos")
+                case _:
+                    continue
+            
