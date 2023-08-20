67b247fc6eaed7193b48ac027d46604a3379f602
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-08-20 19:26:11 +0200

    Adding working CD

--- Data/Explorer.py
+++ Data/Explorer.py
@@ -1,9 +1,17 @@
+import os
+
 class Explorer(object):
     def __init__(self):
         print("Explorer")
         self.Directory = "../"
     def ChangeDirectory(self):
+        print("CD needs to come from Filespot.py directory")
         Change = str(input("Change Directory: "))
-        self.Directory = Change
+        Check = self.Directory + Change
+        if os.path.isdir(Check) == True:
+            print("Directory changed")
+            self.Directory = Check
+        else:
+            print("Directory not found")
     def GetDirectory(self):
         print("Current Directory: ", self.Directory)
