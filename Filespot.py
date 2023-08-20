e23d79715ee338580c66453fcb1d1b09a4abbddf
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-08-20 12:59:20 +0200

    Adding Test Explorer buttons

--- Filespot.py
+++ Filespot.py
@@ -4,10 +4,12 @@ from tkinter import ttk
 from Data.StartUp import StartUp
 from Data.Folders import Folders
 from Data.Files import Files
+from Data.Explorer import Explorer
 
 Start = StartUp()
 Folders = Folders()
 Files = Files()
+Explorer = Explorer()
 
 Filespot = Tk()
 Filespot.title("Filespot")
@@ -18,9 +20,15 @@ Style.theme_use("clam")
 Frame = ttk.Frame(Filespot)
 Frame.grid()
 
-ttk.Button(Frame, text="Sort", command=lambda: Files.Sort()).grid(column = 1, row = 0)
-ttk.Button(Frame, text="Create Folders", command=lambda: Folders.Create()).grid(column = 2, row = 0)
-ttk.Button(Frame, text="List Files", command=lambda: Files.List()).grid(column = 1, row = 1)
-ttk.Button(Frame, text="Quit", command=lambda: Filespot.destroy()).grid(column = 2, row = 1)
+ttk.Button(Frame, text = "Sort", command=lambda: Files.Sort()).grid(column = 1, row = 0)
+ttk.Button(Frame, text = "Create Folders", command=lambda: Folders.Create()).grid(column = 2, row = 0)
+ttk.Button(Frame, text = "List Files", command=lambda: Files.List()).grid(column = 1, row = 1)
+ttk.Button(Frame, text = "Quit", command=lambda: Filespot.destroy()).grid(column = 2, row = 1)
+
+ttk.Button(Frame, text = "").grid(column = 1, row = 2)
+ttk.Button(Frame, text = "").grid(column = 2, row = 2)
+
+ttk.Button(Frame, text = "CD", command=lambda: Explorer.ChangeDirectory()).grid(column = 1, row = 3)
+ttk.Button(Frame, text = "Get Directory", command=lambda: Explorer.GetDirectory()).grid(column = 2, row = 3)
 
 Filespot.mainloop()
