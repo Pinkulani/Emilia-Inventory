4a3782e1d5ed47489dd2db6c6d4f0a8421df7f38
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-08-20 20:18:01 +0200

    Adding file listing

--- Data/Explorer.py
+++ Data/Explorer.py
@@ -4,6 +4,8 @@ class Explorer(object):
     def __init__(self):
         print("Explorer")
         self.Directory = "../"
+        self.FileList = os.listdir(self.Directory)
+    
     def ChangeDirectory(self):
         print("CD needs to come from Filespot.py directory")
         Change = str(input("Change Directory: "))
@@ -13,5 +15,15 @@ class Explorer(object):
             self.Directory = Check
         else:
             print("Directory not found")
+    
     def GetDirectory(self):
         print("Current Directory: ", self.Directory)
+    
+    def UpdateFileList(self):
+        self.FileList = os.listdir(self.Directory)
+
+    def List(self):
+        print("List of Files in Directory: ")
+        self.UpdateFileList()
+        for X in range(0, len(self.FileList)):
+            print(self.FileList[X])
